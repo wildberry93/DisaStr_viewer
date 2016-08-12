f83a50318b3bb1869395b5dbb444f1b1691b36c2
Jagoda Jabłońska <Jagoda Jaboska>
2016-08-12 15:24:08 +0100

    zooming and selecting residues

--- disastr_plugin.js
+++ disastr_plugin.js
@@ -1,6 +1,8 @@
 var glviewer = null;
 var labels = [];
 var pdb_id = "1a5j";
+var list = ["Arg20Ser", "His30Thr", "Met111His", "Ala2002Lys"];
+
 
 var addLabels = function() {
 	var atoms = glviewer.getModel().selectedAtoms({
@@ -79,17 +81,49 @@ var readText = function(input,func) {
 };
 
 var muts = [2,5,10]
+var variants = [3,20]
+
+function make_muts_list(element, mut, glelement) {
+	//var myRe = new RegExp("([A-Za-z]{3})([0-9]+)([A-Za-z]{3}|\?)", "g");
+	//var myArray = myRe.exec("Ala20His"); // get residue number 
+	resi_num = 20
+	//string = '<li onclick="switchColors(this,'+resi_num+','+glelement+');">'+mut+'</li>';
+	string = '<li onclick="switchColors(this,'+20+',glviewer);">'+mut+'</li>';
+	element.append(string);
+}
+
+function switchColors(element, position, glelement) {  
+
+	//links=document.getElementsByTagName("li"); 
+	glelement.setStyle({resi: position}, {stick: {color: 'blue'}});
+	glviewer.zoomTo({resi: position});
+	glelement.render();
+
+	/*for (var i = 0 ; i < links.length ; i ++)  
+	links.item(i).style.color = 'black';  
+	element.style.color='orange'; */
+}  
+
 
 $(document).ready(function() {
 
 	glviewer = $3Dmol.createViewer("gldiv");
 
-    $3Dmol.download('pdb:'+pdb_id, glviewer, {}, function(){
+	for (var i = 0; i < list.length; i++) {
+    	make_muts_list( $("#selediv"), list[i], glviewer);
+	}
+
+
+    $3Dmol.download('pdb:'+pdb_id, glviewer, {}, function() {
     	glviewer.setStyle({}, {cartoon:{}}); 
-    	glviewer.setStyle({resi: 5}, {stick: {}}); // muts resis
+    	glviewer.setStyle({resi: muts}, {cartoon: {color: 'green'}}); // muts resis
+    	glviewer.setStyle({resi: variants}, {cartoon: {color: 'red'}}); // variants resis
     	glviewer.render();
     });
-    	
+
+
+
+
 	glviewer.setBackgroundColor(0xffffff);
 
 });
